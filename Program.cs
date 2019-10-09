4cd0459ef0a57c9e304d29c17327cbd2c357b983
Samuel <spmadeira@edu.unifor.br>
2019-10-08 23:28:56 -0300

    Expor IP para uso da API

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace TrabalhoSistemas
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:80")
                 .UseStartup<Startup>();
     }
 }
